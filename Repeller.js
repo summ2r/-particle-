882eece74258589e4cf8bf8bf213b112844181de
summ2r <out08g16@naver.com>
2024-11-26 14:37:02 +0900

    repeller 클래스에 repel 업데이트

--- Repeller.js
+++ Repeller.js
@@ -4,6 +4,23 @@ class Repeller {
         this.power = 150;
     }
 
+    // All the same steps to calculate an attractive force, only pointing in the opposite direction
+     repel(particle) {
+        // Step 1: Get the force direction.
+        let force = p5.Vector.sub(this.position, particle.position);
+
+        // Step 2: Get and constrain the distance.
+        let distance = force.mag();
+        distance = constrain(distance, 5, 50);
+      
+        // Step 3: Calculate the magnitude, using a power variable for G.
+        let strength = -1 * this.power / (distance * distance);
+    
+        // Step 4: Make a vector out of the direction and magnitude.
+        force.setMag(strength);
+        return force;
+  }
+
     show() {
         stroke(0);
         FileList(127);
