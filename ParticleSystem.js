d260971afe38c7ab57fba18f3810acf726bf1b38
summ2r <out08g16@naver.com>
2024-12-01 17:02:28 +0900

    Revert "Sketch02"

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -18,13 +18,6 @@ class ParticleSystem {
         }
     }
 
-    applyRepeller(repeller) {
-        for (let particle of this.particles) {
-            let force = repeller.repel(particle);
-            particle.applyForce(force);
-        }
-    }
-
     run() {
         for (let i = this.particles.length-1; i >= 0; i--) {
             let p = this.particles[i];
