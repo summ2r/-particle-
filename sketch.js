b0c3d71473e204e561968bc973c6527865346e8e
summ2r <out08g16@naver.com>
2024-12-03 14:45:39 +0900

    텍스트 예제 시도 (실패)

--- sketch.js
+++ sketch.js
@@ -1,15 +1,41 @@
 let systems = [];
 let repeller;
+let input, button, greeting;
 
 let g; // gravity
-// let wind;
 
 function setup() {
   createCanvas(720, 400);
-  // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
-  // wind = createVector(0.03, -0.01);
   repeller = new Repeller(0, height / 2);
+  
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('sss');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'whAt iS yOUr nAmE?');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function greet() {
+  const name = input.value();
+  greeting.html('hello ' + name + '!');
+  input.value('');
+
+  for (let i = 0; i < 200; i++) {
+    push();
+    fill(random(255), 255, 255);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(name, 0, 0);
+    pop();
+  }
 }
 
 function draw() {
