25f248b7e5f91c240ee8edb0fd66a7a6e9f90010
summ2r <out08g16@naver.com>
2024-12-08 23:37:49 +0900

    Positioning Objects in a Scene 예제 업데이트

--- sketch.js
+++ sketch.js
@@ -1,16 +1,50 @@
 let systems = [];
 let repeller;
+let positions = [];
+let rotations = [];
+let scales = [];
 
 let g; // gravity
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(720, 400, WEBGL);
+  for (let i = 0; i < 10; i++) {
+    positions.push(createVector(
+      random(-width / 2, width / 2),
+      random(-height / 2, height / 2),
+      random(-width / 2, width / 2)
+    ));
+    rotations.push(createVector(
+      random(TWO_PI),
+      random(TWO_PI),
+      random(TWO_PI)
+    ));
+    scales.push(random(0.5, 2));
+  }
   g = createVector(0, -0.02);
   repeller = new Repeller(0, height / 2);
 }
 
 function draw() {
   background(255);
+  orbitControl();
+  noStroke();
+  lights();
+
+  for (let i = 0; i < 10; i += 1) {
+    push();
+    translate(
+      positions[i].x,
+      positions[i].y,
+      positions[i].z
+    );
+    rotateX(rotations[i].x);
+    rotateY(rotations[i].y);
+    rotateZ(rotations[i].z);
+    scale(scales[i]);
+    box(20);
+    pop();
+  }
 
   for (let s of systems) {
     s.addParticle();
